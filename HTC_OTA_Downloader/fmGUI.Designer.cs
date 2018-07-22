5bc6b54b880a6bac14970cd7ef9348d86aa97d94
YU-YEN HSU <sabpprook@gmail.com>
2018-07-22 22:10:17 +0800

    first commit

---
+++ HTC_OTA_Downloader/fmGUI.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace HTC_OTA_Downloader
+{
+    partial class fmGUI
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox = new System.Windows.Forms.GroupBox();
+            this.text_cidnum = new System.Windows.Forms.TextBox();
+            this.text_version = new System.Windows.Forms.TextBox();
+            this.text_model = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_execute = new System.Windows.Forms.Button();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.text_json = new System.Windows.Forms.TextBox();
+            this.checkBox = new System.Windows.Forms.CheckBox();
+            this.groupBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox
+            // 
+            this.groupBox.Controls.Add(this.text_cidnum);
+            this.groupBox.Controls.Add(this.text_version);
+            this.groupBox.Controls.Add(this.text_model);
+            this.groupBox.Controls.Add(this.label3);
+            this.groupBox.Controls.Add(this.label2);
+            this.groupBox.Controls.Add(this.label1);
+            this.groupBox.Font = new System.Drawing.Font("Verdana", 11.25F);
+            this.groupBox.Location = new System.Drawing.Point(12, 13);
+            this.groupBox.Margin = new System.Windows.Forms.Padding(4);
+            this.groupBox.Name = "groupBox";
+            this.groupBox.Padding = new System.Windows.Forms.Padding(4);
+            this.groupBox.Size = new System.Drawing.Size(410, 144);
+            this.groupBox.TabIndex = 0;
+            this.groupBox.TabStop = false;
+            this.groupBox.Text = "Information";
+            // 
+            // text_cidnum
+            // 
+            this.text_cidnum.Location = new System.Drawing.Point(135, 104);
+            this.text_cidnum.Name = "text_cidnum";
+            this.text_cidnum.Size = new System.Drawing.Size(250, 26);
+            this.text_cidnum.TabIndex = 5;
+            this.text_cidnum.Text = "HTC__621";
+            this.text_cidnum.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // text_version
+            // 
+            this.text_version.Location = new System.Drawing.Point(135, 66);
+            this.text_version.Name = "text_version";
+            this.text_version.Size = new System.Drawing.Size(250, 26);
+            this.text_version.TabIndex = 4;
+            this.text_version.Text = "2.41.709.73";
+            this.text_version.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // text_model
+            // 
+            this.text_model.Location = new System.Drawing.Point(135, 28);
+            this.text_model.Name = "text_model";
+            this.text_model.Size = new System.Drawing.Size(250, 26);
+            this.text_model.TabIndex = 3;
+            this.text_model.Text = "htc_pmeuhl";
+            this.text_model.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(8, 107);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(42, 18);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "CID:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(8, 69);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 18);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Version:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Verdana", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(8, 31);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Model:";
+            // 
+            // btn_execute
+            // 
+            this.btn_execute.Font = new System.Drawing.Font("Verdana", 11.25F);
+            this.btn_execute.Location = new System.Drawing.Point(12, 165);
+            this.btn_execute.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_execute.Name = "btn_execute";
+            this.btn_execute.Size = new System.Drawing.Size(280, 50);
+            this.btn_execute.TabIndex = 1;
+            this.btn_execute.TabStop = false;
+            this.btn_execute.Text = "EXECUTE";
+            this.btn_execute.UseVisualStyleBackColor = true;
+            this.btn_execute.Click += new System.EventHandler(this.btn_execute_Click);
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(12, 459);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(410, 40);
+            this.progressBar.TabIndex = 2;
+            // 
+            // text_json
+            // 
+            this.text_json.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.text_json.Location = new System.Drawing.Point(12, 222);
+            this.text_json.Multiline = true;
+            this.text_json.Name = "text_json";
+            this.text_json.ReadOnly = true;
+            this.text_json.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.text_json.Size = new System.Drawing.Size(410, 231);
+            this.text_json.TabIndex = 6;
+            this.text_json.TextChanged += new System.EventHandler(this.text_json_TextChanged);
+            // 
+            // checkBox
+            // 
+            this.checkBox.AutoSize = true;
+            this.checkBox.Location = new System.Drawing.Point(305, 165);
+            this.checkBox.Name = "checkBox";
+            this.checkBox.Size = new System.Drawing.Size(124, 22);
+            this.checkBox.TabIndex = 7;
+            this.checkBox.Text = "China Varient";
+            this.checkBox.UseVisualStyleBackColor = true;
+            // 
+            // fmGUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(434, 511);
+            this.Controls.Add(this.checkBox);
+            this.Controls.Add(this.text_json);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.btn_execute);
+            this.Controls.Add(this.groupBox);
+            this.Font = new System.Drawing.Font("Verdana", 11.25F);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "fmGUI";
+            this.Text = "HTC OTA Downloader";
+            this.SizeChanged += new System.EventHandler(this.fmGUI_SizeChanged);
+            this.groupBox.ResumeLayout(false);
+            this.groupBox.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox;
+        private System.Windows.Forms.Button btn_execute;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox text_cidnum;
+        private System.Windows.Forms.TextBox text_version;
+        private System.Windows.Forms.TextBox text_model;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.TextBox text_json;
+        private System.Windows.Forms.CheckBox checkBox;
+    }
+}
+
---
+++ HTC_OTA_Downloader/fmGUI.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Script.Serialization;
+using System.Windows.Forms;
+
+namespace HTC_OTA_Downloader
+{
+    public class OTA_Object
+    {
+        public string time_msec { get; set; }
+        public bool stats_ok { get; set; }
+        public string reason { get; set; }
+        public List<Intent> intent { get; set; }
+        public class Intent
+        {
+            public string data_uri { get; set; }
+            public string enc_type { get; set; }
+            public string pkgFileName { get; set; }
+            public string secure_uri { get; set; }
+        }
+    }
+
+    public partial class fmGUI : Form
+    {
+        readonly string CHECKIN_URL = "https://andchin-2.htc.com/htcfotacheckin/rest/checkin";
+        readonly string CHECKIN_URL_CN = "https://andchin-2.htccomm.com.cn/htcfotacheckin/rest/checkin";
+        readonly string SN = "CC55TYG07414";
+        readonly string IMEI = "357290062810490";
+        long TimeStamp = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
+
+        public fmGUI()
+        {
+            InitializeComponent();
+        }
+
+        private void fmGUI_SizeChanged(object sender, EventArgs e)
+        {
+            var height = this.Height;
+            var width = this.Width;
+            btn_execute.Width = width - 170;
+            groupBox.Width = text_json.Width = progressBar.Width = width - 40;
+            text_model.Width = text_version.Width = text_cidnum.Width = Width - 200;
+            text_json.Height = height - 320;
+            progressBar.Location = new Point(12, height - 90);
+            checkBox.Location = new Point(width - 145, 165);
+        }
+
+        private void text_json_TextChanged(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(text_json.Text))
+            {
+                text_json.Text = text_json.Text.Replace("[{", "[\r\n{")
+                                               .Replace("}]", "}\r\n]")
+                                               .Replace("{\"", "{\r\n\"")
+                                               .Replace("\"}", "\"\r\n}")
+                                               .Replace(",\"", ",\r\n\"")
+                                               .Replace(",{", ",\r\n{")
+                                               .Replace("\"},", "\"\r\n},");
+            }
+        }
+
+        private void btn_execute_Click(object sender, EventArgs e)
+        {
+            using (var web = new WebClient())
+            {
+                var headers = new WebHeaderCollection();
+                headers.Add(HttpRequestHeader.UserAgent, "Android-Checkin/8.0");
+                headers.Add(HttpRequestHeader.ContentType, "application/json");
+                headers.Add("x-active-g", "DivadGS38Omatump76");
+                web.Headers = headers;
+
+                TimeStamp = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
+                var model = text_model.Text;
+                var version = text_version.Text;
+                var cidnum = text_cidnum.Text;
+                var post_string = $"{{\"imei\":\"{IMEI}\",\"timeStamp\":{TimeStamp},\"model_number\":\"{model}\",\"checkin\":{{\"build\":{{\"serialno\":\"{SN}\",\"firmware_version\":\"{version}\"}},\"client_version\":\"A8.0(M)\",\"cid\":\"{cidnum}\",\"checkin_type\":\"Manual\"}},\"x1\":\"{getX1()}\"}}";
+                var response_string = web.UploadString(checkBox.Checked ? CHECKIN_URL_CN : CHECKIN_URL, post_string);
+                text_json.Text = response_string;
+
+                var serializer = new JavaScriptSerializer();
+                var OTA = serializer.Deserialize<OTA_Object>(response_string);
+
+                if (OTA.intent != null && OTA.intent[0].data_uri != null)
+                {
+                    var url = OTA.intent[0].data_uri;
+                    var pkgname = OTA.intent[0].pkgFileName;
+                    if (MessageBox.Show(pkgname, "Download?", MessageBoxButtons.OKCancel, MessageBoxIcon.Asterisk) == DialogResult.OK)
+                    {
+                        headers = new WebHeaderCollection();
+                        headers.Add(HttpRequestHeader.UserAgent, "Android-Checkin/8.0");
+                        headers.Add("htc1s", getHTC1S(long.Parse(OTA.time_msec)));
+                        web.Headers = headers;
+
+                        web.DownloadProgressChanged += (_s, _e) => progressBar.Value = _e.ProgressPercentage;
+                        web.DownloadFileCompleted += (_s, _e) =>
+                        {
+                            this.Text = "HTC OTA Downloader";
+                            System.Media.SystemSounds.Asterisk.Play();
+                            progressBar.Value = 0;
+                        };
+
+                        this.Text = pkgname;
+                        web.DownloadFileAsync(new Uri(url), pkgname);
+                    }
+                }
+            }
+        }
+
+        private string getX1()
+        {
+            string time = TimeStamp.ToString();
+            int shift_value = 0;
+            for (int i = 4; i <= time.Length; i++)
+            {
+                if (time[time.Length - i] != '0')
+                {
+                    shift_value = (int)char.GetNumericValue(time[time.Length - i]);
+                    break;
+                }
+            }
+            var time_head = time.Substring(0, time.Length - shift_value);
+            var time_tail = time.Substring(time.Length - shift_value);
+            return sha256(time_tail + SN + IMEI + time_head);
+        }
+
+        public string getHTC1S(long timestamp)
+        {
+            var htc1s = SN + timestamp;
+            if (timestamp <= 0) return htc1s;
+            for (long i = timestamp % 10; ; i = timestamp % 10)
+            {
+                if (i == 0)
+                    timestamp /= 10;
+                else
+                {
+                    var index = htc1s.Length - (int)i;
+                    return htc1s.Substring(index) + htc1s.Remove(index);
+                }
+            }
+        }
+
+        private string sha256(string str)
+        {
+            var bytes = Encoding.ASCII.GetBytes(str);
+            var data = SHA256.Create().ComputeHash(bytes);
+            return BitConverter.ToString(data).Replace("-", "").ToUpper();
+        }
+    }
+}
